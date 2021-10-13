a3b8ea353b638ad52f978c153116ab96149728d2
toanht <toanht@allogical.com>
2021-10-13 22:05:10 +0700

    add: Add file Bai3 .js

--- JS/Bai3.js
+++ JS/Bai3.js
@@ -4,7 +4,7 @@ function compareDate(date1,date2){
     if(myDate1>myDate2){
         console.log("date 1 lon hon date 2")
     }else {
-        console.log("date 1 nho lon hon date 2")
+        console.log("date 1 nho  hon date 2")
     }
 }
 compareDate('2020/09/09 11:00:00','2021/10/01 10:00:00')
