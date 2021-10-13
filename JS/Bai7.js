59d524e907f06b11bf985d2b11d06b5a5ecbddc5
toanht <toanht@allogical.com>
2021-10-13 10:09:08 +0700

    add: Add file Bai 7 .js

--- JS/Bai7.js
+++ JS/Bai7.js
@@ -1,13 +1,12 @@
 let myArray=[2,42,5,42,304,1,13,2,13];
-myArray.sort(function(a, b) {
-    return a - b;
-});
 const newArray= (myArray) => {
+    myArray.sort(function(a, b) {
+        return a - b;
+    });
     let value=myArray[0];
     for(let i = 1;i< myArray.length-1;i++){
         if(value===myArray[i]){
             myArray.splice(i,1);
-            console.log(myArray);
         }
         value=myArray[i];
         
