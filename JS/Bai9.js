73d37d379aa6950e22f0c224014d9110dbac2b9a
toanht <toanht@allogical.com>
2021-10-13 22:13:56 +0700

    add: Add file Bai 9 .js

--- JS/Bai9.js
+++ JS/Bai9.js
@@ -1,7 +1,12 @@
 function newArr(myArray,findValue){
+    if(myArray.includes(findValue)){
     let filterResult = myArray.filter(function(element){
-        return element !== findValue;
+        return element!==findValue;
+       
     });
     return filterResult;
+    }else{
+        return "Khong tim thay gia tri";
+    }
 }
-console.log(newArr([2,4,6,8],6));
+console.log(newArr([2,4,6,8],7));
